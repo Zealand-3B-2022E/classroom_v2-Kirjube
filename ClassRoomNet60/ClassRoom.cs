af6ec1eca6ffb848103c20b9d6695332d1ac9986
Kirstine Juncker Berantzino <kirs084p@edu.zealand.dk>
2022-09-05 13:37:29 +0200

    Klasseværelse + Student -klasse

---
+++ ClassRoomNet60/ClassRoom.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoomNet60
+{
+    internal class ClassRoom
+    {
+        private String _className;
+        private  List<Student> _classList;
+        private DateTime _semesterStart;
+
+        ClassRoom()
+        {
+            _className = "Ikke valgt";
+            _classList = new List<Student>();
+            _semesterStart = DateTime.Now;
+
+        }
+
+
+    }
+}
---
+++ ClassRoomNet60/Student.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoomNet60
+{
+    internal class Student
+    {
+        private string _name;
+        private int _birthMonth;
+        private int _birthDay;  
+
+        public Student(string name, int birthMonth, int birthDay)
+        {
+            _name = name;
+            _birthMonth = birthMonth;
+            _birthDay = birthDay;
+
+        }
+
+        public String Name { get; set; }
+        public int BirthMonth { get; set; }
+        public int BirthDay { get; set; }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
